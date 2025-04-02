662262da472fec29d3e16b93dde9dfaa28b86fb3
sebu <84217335+kimsebu77@users.noreply.github.com>
2025-04-02 19:32:32 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+            System.out.println(a/b);
+    }
+}
